5085d4e8fdc410d091735866b0ccd2b92b62ef3b
joshuaxu71 <joshuaxu71@gmail.com>
2024-09-07 07:40:59 +0700

    update /ban_history to only work for a specific user

--- src/commands/banHistory.js
+++ src/commands/banHistory.js
@@ -7,36 +7,31 @@ const banActionService = new BanActionService();
 module.exports = {
    data: new SlashCommandBuilder()
       .setName("ban_history")
-      .setDescription("Lists the ban history of a specific user or the server.")
+      .setDescription("Lists the ban history of a specific user.")
       .addUserOption((option) =>
          option
             .setName("user")
             .setDescription("The user whose ban history you want to view")
-            .setRequired(false)
+            .setRequired(true)
       ),
    async execute(interaction) {
       const user = interaction.options.getUser("user");
-      let banActions;
-      if (user) {
-         banActions = await banActionService.getBanActionsByGuildIdAndUserId(
-            interaction.guildId,
-            user.id
-         );
-      } else {
-         banActions = await banActionService.getBanActionsByGuildId(interaction.guildId);
-      }
+      const banActions = await banActionService.getBanActionsByGuildIdAndUserId(
+         interaction.guildId,
+         user.id
+      );
 
       let banHistory = [];
       if (banActions.length) {
          for (const banAction of banActions) {
             banHistory.push(
-               `Date: ${banAction.createdDate}\nActor ID: ${banAction.actorId}\nActor Username: ${banAction.actorUsername}\nUser ID: ${banAction.userId}\nUser Username: ${banAction.userUsername}\nNote: ${banAction.note}`
+               `Date: ${banAction.createdDate}\nActor ID: ${banAction.actorId}\nActor Username: ${banAction.actorUsername}\nNote: ${banAction.note}`
             );
          }
       }
 
       if (!banActions.length) {
-         banHistory.push("There are no bans yet.");
+         banHistory.push("There are no ban actions taken against this user yet.");
       }
 
       await interaction.reply({
